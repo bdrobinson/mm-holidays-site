7c51b28009ee6d537e8750fb7187f2c84b731bab
Ben Robinson <ben@joinfoodchain.com>
2018-11-27 21:40:07 +0000

    Sort out manifest/favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,13 +16,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: "gatsby-starter-default",
-        short_name: "starter",
+        name: "M+M Holidays",
+        short_name: "M+M Holidays",
         start_url: "/",
         background_color: "#663399",
         theme_color: "#663399",
-        display: "minimal-ui",
-        // icon: "src/images/gatsby-icon.png", // This path is relative to the root of the site.
+        display: "standalone",
+        icon: "src/images/logo_black_square.png",
       },
     },
     "gatsby-plugin-styled-components",
