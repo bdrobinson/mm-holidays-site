87aa51f78df0076846ce5a470207ac5b5e937f7b
Ben Robinson <ben@joinfoodchain.com>
2019-01-07 14:43:32 +0000

    Add fetch polyfill

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -6,6 +6,7 @@
 
 import { setConfig } from "react-hot-loader"
 import * as Sentry from "@sentry/browser"
+import "unfetch/polyfill"
 
 export const onClientEntry = () => {
   // Needed to make react hooks work with HMR
